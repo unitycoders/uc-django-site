477605de477c6d1efb5676ae12976e636d51c951
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2014-12-29 03:04:55 +0000

    admin improvements

--- lectern/admin.py
+++ lectern/admin.py
@@ -4,9 +4,13 @@ from models import Article, RelatedLink
 # Register your models here.
 class RelatedLinkInline(admin.TabularInline):
 	model = RelatedLink
+	extra = 1
 
 class ArticleAdmin(admin.ModelAdmin):
 	prepopulated_fields = {"slug": ("title",)}
+	search_fields = ['title']
+	list_display = ['__unicode__', 'slug']
+	list_filter = ['created', 'modified']
 	inlines = [
 		RelatedLinkInline
 	]
