869e5891adc0f693ceba22f781d0d87588f0be71
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2014-12-29 01:43:05 +0000

    django rest api setup

--- unitycoders/urls.py
+++ unitycoders/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +10,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # REST API stuff
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
