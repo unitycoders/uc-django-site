b7a1bbb0fdea433b3515df8bc50ee3d1553fac77
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2015-04-03 17:04:15 +0100

    home page I don't remember coding... but apprently did

--- unitycoders/urls.py
+++ unitycoders/urls.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 from rest_framework import routers
 
 from lectern.views import ArticleViewSet
+from unitycoders.views import HomeView
 
 router = routers.DefaultRouter()
 router.register(r'articles', ArticleViewSet)
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'unitycoders.views.home', name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^articles/', include('lectern.urls', namespace='articles', app_name="lectern")),
