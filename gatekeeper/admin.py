8910c27753f8433146fc1df3f55a6dac3b7a9267
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2015-04-07 16:17:15 +0100

    gatekeeper admin improvements

--- gatekeeper/admin.py
+++ gatekeeper/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from django.utils.translation import ugettext, ugettext_lazy as _
 from models import Member,Invite
 
-class MemberManager(admin.ModelAdmin):
-	search_fields = ['username', 'first_name', 'last_name']
-	list_display = ['username', 'email'] 
-	pass
+# based on https://github.com/django/django/blob/master/django/contrib/auth/admin.py
+class MemberManager(UserAdmin):
+	 fieldsets = (
+		(None, {
+			'fields': ('username', 'password')
+		}),
+		(_('Personal info'), {
+				'fields': (('first_name', 'last_name'), 'email', 'bio')
+		}),
+		(_('Permissions'), {
+				'classes': ('collapse',),
+				'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+		}),
+		(_('Important dates'), {
+				'classes': ('collapse',),
+				'fields': ('last_login', 'date_joined')
+		}),
+	)
+
 admin.site.register(Member, MemberManager)
 
 class InviteManager(admin.ModelAdmin):
