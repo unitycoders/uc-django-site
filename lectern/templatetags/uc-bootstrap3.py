c81e8829bda58b2713ae70ba2dbceb3b15eb5354
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2015-05-22 20:23:07 +0100

    added the ability to put hover text on fa icons

--- lectern/templatetags/uc-bootstrap3.py
+++ lectern/templatetags/uc-bootstrap3.py
@@ -3,5 +3,6 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def fa_icon(name):
-    return "<i class=\"fa fa-%s\" aria-hidden=\"true\"></i>" % (name,)
+def fa_icon(name, title=None):
+    tmp = ('title="%s"' % title) if title else ""
+    return "<i class=\"fa fa-%s\" aria-hidden=\"true\" %s></i>" % (name,tmp)
